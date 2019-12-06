744296ea2e5be315b49ceb09a682124cb4aec76d
Fred Baube <fbaube@welho.com>
2019-12-06 20:18:01 +0200

    Better docu.

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	// "fmt"
 	"github.com/fbaube/bloggenator/cli"
+	// "github.com/morningconsult/serrors"
 )
 
 // main is a dead simple one-liner eh ?
 func main() {
+	/*
+	s := serrors.New("This is a stack trace error")
+	fmt.Printf("====\n%+v\n====\n",s)
+	callStack, _ := serrors.Trace(s, serrors.StandardFormat)
+  fmt.Println(callStack, "===")
+	*/
 	cli.Run()
 }
